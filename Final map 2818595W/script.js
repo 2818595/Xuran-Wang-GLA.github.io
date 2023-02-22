adea349a872747b69f60609d3305b96ddb37dba9
2818595 <122904213+2818595@users.noreply.github.com>
2023-02-22 21:15:52 +0000

    Final map

---
+++ Final map 2818595W/script.js
@@ -0,0 +1,301 @@
+var dataArr = [
+  {
+    title: "Buger king",
+    id: 1,
+    list: [
+      { Name: "Buger king", Postalcode: "92869" },
+      { Name: "Buger king", Postalcode: "92841"},
+      { Name: "Buger king", Postalcode: "92833" },
+      { Name: "Buger king", Postalcode: "92683" },
+      { Name: "Buger king", Postalcode: "92553" },
+      { Name: "Buger king", Postalcode: "92503" },
+      { Name: "Buger king", Postalcode: "92223" },
+      { Name: "Buger king", Postalcode: "92128" },
+      
+    ]
+  },
+
+  {
+    title: "Carl's Jr.",
+    id: 2,
+    list: [
+      { Name: "Carl's Jr.", Postalcode: "92111" },
+      { Name: "Carl's Jr.", Postalcode: "92102" },
+      { Name: "Carl's Jr.", Postalcode: "92081" },
+      { Name: "Carl's Jr.", Postalcode: "92021" },
+      { Name: "Carl's Jr.", Postalcode: "92020" },
+      { Name: "Carl's Jr.", Postalcode: "91950" },
+      { Name: "Carl's Jr.", Postalcode: "91748" },
+      { Name: "Carl's Jr.", Postalcode: "91741" },
+      { Name: "Carl's Jr.", Postalcode: "91733" },
+      { Name: "Carl's Jr.", Postalcode: "91731" },
+      { Name: "Carl's Jr.", Postalcode: "91755" },
+      { Name: "Carl's Jr.", Postalcode: "91324" },
+      { Name: "Carl's Jr.", Postalcode: "91321" },
+      { Name: "Carl's Jr.", Postalcode: "91320" },
+ ]
+  },
+  {
+    title: "Del Taco",
+    id: 3,
+    list: [
+      { Name: "Del Taco", Postalcode: "90712" },
+      { Name: "Del Taco", Postalcode: "90631" },
+      { Name: "Del Taco", Postalcode: "90401" },
+      { Name: "Del Taco", Postalcode: "90275" },
+      { Name: "Del Taco", Postalcode: "90029" },
+      { Name: "Del Taco", Postalcode: "90007" },
+      { Name: "Del Taco", Postalcode: "92868" },
+      
+      
+    ]
+  },
+  {
+    title: "El Pollo Loco",
+    id: 4,
+    list: [
+      { Name: "El Pollo Loco", Postalcode: "92801" },
+      { Name: "El Pollo Loco", Postalcode: "92545" },
+      { Name: "El Pollo Loco", Postalcode: "92309" },
+      { Name: "El Pollo Loco", Postalcode: "92211" },
+      { Name: "El Pollo Loco", Postalcode: "92173" },
+      { Name: "El Pollo Loco", Postalcode: "92120" },
+      { Name: "El Pollo Loco", Postalcode: "92021" },
+      { Name: "El Pollo Loco", Postalcode: "91942" },
+      { Name: "El Pollo Loco", Postalcode: "91761" },
+      { Name: "El Pollo Loco", Postalcode: "91343" },
+      { Name: "El Pollo Loco", Postalcode: "91205" },
+     
+      
+    ]
+  },
+  {
+    title: "Jack in the Box",
+    id: 5,
+    list: [
+{ Name: "Jack in the Box", Postalcode: "91006" },
+{ Name: "Jack in the Box",	Postalcode: "90713" },
+{ Name: "Jack in the Box",	Postalcode: "90640" },
+{ Name: "Jack in the Box",	Postalcode: "90249" },
+{ Name: "Jack in the Box",	Postalcode: "90241" },
+{ Name: "Jack in the Box",	Postalcode: "90063" },
+{ Name: "Jack in the Box",	Postalcode: "90028" },
+{ Name: "Jack in the Box",	Postalcode: "92879" },
+{ Name: "Jack in the Box",	Postalcode: "92866" },
+{ Name: "Jack in the Box",	Postalcode: "92806" },
+{ Name: "Jack in the Box",	Postalcode: "92804" },
+{ Name: "Jack in the Box",	Postalcode: "92801" },
+{ Name: "Jack in the Box",	Postalcode: "92705" },
+{ Name: "Jack in the Box",	Postalcode: "92688" },
+{ Name: "Jack in the Box",	Postalcode: "92660" },
+{ Name: "Jack in the Box",	Postalcode: "92630" },
+{ Name: "Jack in the Box",	Postalcode: "92626" },
+{ Name: "Jack in the Box",	Postalcode: "92612" },
+
+    ]
+  },
+  {
+    title: "Pizza Hut",
+    id: 6,
+    list: [
+{ Name: "Pizza Hut",  Postalcode: "90062" },
+{ Name: "Pizza Hut",	Postalcode: "90002" },
+{ Name: "Pizza Hut",	Postalcode: "92886" },
+{ Name: "Pizza Hut",	Postalcode: "92869" },
+{ Name: "Pizza Hut",	Postalcode: "92804" },
+
+    ]
+  },
+  {
+    title: "Taco Bell",
+    id: 7,
+    list: [
+{ Name: "Taco Bell",  Postalcode: "91761" },
+{ Name: "Taco Bell",	Postalcode: "91744" },
+{ Name: "Taco Bell",	Postalcode: "91504" },
+{ Name: "Taco Bell",	Postalcode: "91405" },
+{ Name: "Taco Bell",	Postalcode: "91362" },
+{ Name: "Taco Bell",	Postalcode: "91355" },
+{ Name: "Taco Bell",	Postalcode: "91342" },
+{ Name: "Taco Bell",	Postalcode: "91331" },
+{ Name: "Taco Bell",	Postalcode: "91304" },
+{ Name: "Taco Bell",	Postalcode: "91105" },
+{ Name: "Taco Bell",	Postalcode: "91104" },
+{ Name: "Taco Bell",	Postalcode: "90805" },
+{ Name: "Taco Bell",	Postalcode: "90731" },
+{ Name: "Taco Bell",	Postalcode: "90703" },
+{ Name: "Taco Bell",	Postalcode: "90631" },
+{ Name: "Taco Bell",	Postalcode: "90630" },
+
+
+    ]
+  },
+  {
+    title: "Subway",
+    id: 8,
+    list: [
+{ Name: "Subway", Postalcode: "92705" },
+{ Name: "Subway",	Postalcode: "92663" },
+{ Name: "Subway",	Postalcode: "92660" },
+{ Name: "Subway",	Postalcode: "92626" },
+{ Name: "Subway",	Postalcode: "92562" },
+{ Name: "Subway",	Postalcode: "92407" },
+{ Name: "Subway",	Postalcode: "92395" },
+{ Name: "Subway",	Postalcode: "92373" },
+{ Name: "Subway",	Postalcode: "92236" },
+{ Name: "Subway",	Postalcode: "92234" },
+{ Name: "Subway",	Postalcode: "92203" },
+{ Name: "Subway",	Postalcode: "92083" },
+
+    ]
+  },
+  {
+    title: "Wendy's",
+    id: 9,
+    list: [
+{ Name: "Wendy's",  Postalcode: "90302" },
+{ Name: "Wendy's",	Postalcode: "90248" },
+{ Name: "Wendy's",	Postalcode: "90232" },
+{ Name: "Wendy's",	Postalcode: "90201" },
+{ Name: "Wendy's",	Postalcode: "90031" },
+
+
+    ]
+  },
+  {
+    title: "McDonald's",
+    id: 10,
+    list: [
+      { Name: "McDonald's", Postalcode: "92606" },
+      { Name: "McDonald's", Postalcode: "92508" },
+      { Name: "McDonald's", Postalcode: "92507" },
+      { Name: "McDonald's", Postalcode: "92501" },
+      { Name: "McDonald's", Postalcode: "91373" },
+      { Name: "McDonald's", Postalcode: "92345" },
+      { Name: "McDonald's", Postalcode: "92234" },
+      { Name: "McDonald's", Postalcode: "92220" },
+      { Name: "McDonald's", Postalcode: "92203" },
+      { Name: "McDonald's", Postalcode: "92110" },
+      { Name: "McDonald's", Postalcode: "92083" },
+      { Name: "McDonald's", Postalcode: "92054" },
+      { Name: "McDonald's", Postalcode: "91910" },
+      { Name: "McDonald's", Postalcode: "91752" },
+      { Name: "McDonald's", Postalcode: "91730" },
+      { Name: "McDonald's", Postalcode: "91606" },
+      { Name: "McDonald's", Postalcode: "91406" },
+      { Name: "McDonald's", Postalcode: "91350" },
+      { Name: "McDonald's", Postalcode: "91331" },
+      { Name: "McDonald's", Postalcode: "91325" },
+      { Name: "McDonald's", Postalcode: "90808" },
+      { Name: "McDonald's", Postalcode: "90701" },
+      { Name: "McDonald's", Postalcode: "90631" },
+      { Name: "McDonald's", Postalcode: "90605" },
+      { Name: "McDonald's", Postalcode: "90505" },
+      { Name: "McDonald's", Postalcode: "90302" },
+      { Name: "McDonald's", Postalcode: "90275" },
+      { Name: "McDonald's", Postalcode: "90255" },
+     
+      
+    
+    ]
+  }
+ ];
+(function () {
+mapboxgl.accessToken =
+    "pk.eyJ1IjoiMjgxODU5NXciLCJhIjoiY2xjcWEzc2xyMDBydjN2cDA1ZTR0bGxheCJ9.fSUz674MqXxpw-h14D3q1A"
+
+const map = new mapboxgl.Map({
+  container: 'map', // container element id
+  style: "mapbox://styles/2818595w/clecnnb2e000g01qg38tect7t",
+  center: [-118.15, 34.05],
+  zoom: 10
+});const data_url = "https://api.mapbox.com/datasets/v1/2818595w/clecnnb2e000g01qg38tect7t/features?access_token=pk.eyJ1IjoiMjgxODU5NXciLCJhIjoiY2xjcWEzc2xyMDBydjN2cDA1ZTR0bGxheCJ9.fSUz674MqXxpw-h14D3q1A";
+
+map.on("load", () => {
+ filterType = ["!=", ["get", "name"], "placeholder"];
+  
+map.setFilter("los-angeles", ["All", filterType]);
+  
+document.getElementById("dropdown").addEventListener("change", (event) => {
+  const type = event.target.value;
+  if (type === "All") {
+    filterType = ["!=", ["get", "name"], "All"];
+  } else if (type === "Burger King") {
+    filterType = ["==", ["get", "name"], "Burger King"];
+  } 
+    else if (type === "Carl's Jr.") {
+    filterType = ["==", ["get", "name"], "Carl's Jr."];
+  } 
+  else if (type === "Subway") {
+    filterType = ["==", ["get", "name"], "Subway"];
+  }
+  else if (type === "Taco Bell") {
+    filterType = ["==", ["get", "name"], "Taco Bell"];
+  }
+  else if (type === "Pizza Hut") {
+    filterType = ["==", ["get", "name"], "Pizza Hut"];
+ }  else if (type === "Wendy's") {
+    filterType = ["==", ["get", "name"], "Wendy's"];
+  } else if (type === "El Pollo Loco") {
+    filterType = ["==", ["get", "name"], "El Pollo Loco"];
+  }else if (type === "McDonald's") {
+    filterType = ["==", ["get", "name"], "McDonald's"];
+  } else if (type === "Del Taco") {
+    filterType = ["==", ["get", "name"], "Del Taco"];
+  }
+    else if (type === "Jack in the Box") {
+    filterType = ["==", ["get", "name"], "Jack in the Box"];
+  }else {
+    console.log("error");
+  }
+
+  map.setFilter("los-angeles", ["all", filterType]);
+});
+});
+  function renderMenu(arr) {
+    let listArr = [];
+    listArr.push(`<dl><dt><span>Name</span><span>Postalcode</span></dt>`);
+    arr.forEach((v) => {
+      listArr.push(`<dd><span>${v.Name}</span><span>${v.Postalcode}</span></dd>`);
+    });
+    listArr.push(`</dl>`);
+    return listArr.join("");
+  }
+  let seeListBtn = document.querySelector("#seeListBtn");
+  let menu = document.querySelector("#menu");
+  let menuCloseBtn = document.querySelector("#menuCloseBtn");
+  let menuList = document.querySelector("#menuList");
+  let navList = document.querySelector("#navList");
+  let navListArr = [];
+  dataArr.forEach((v) => {
+    navListArr.push(`<span data-id="${v.id}">${v.title}</span>`);
+  });
+  seeListBtn.addEventListener("click", function () {
+    let classList = menu.classList;
+    let classListArr = Array.from(classList);
+    if (classListArr.indexOf("open") != -1) {
+      classList.remove("open");
+    } else {
+      classList.add("open");
+    }
+  });
+  navList.innerHTML = navListArr.join("");
+  navList.addEventListener("click", function (ev) {
+    let targetDom = ev.target;
+    let getId = targetDom.dataset.id;
+    let getList = dataArr.find((v) => {
+      return v.id == getId;
+    });
+    if (getList) {
+      getList = getList.list;
+    }
+    menuList.innerHTML = renderMenu(getList);
+  });
+  menuCloseBtn.addEventListener("click", function () {
+    menu.classList.remove("open");
+  });
+})();
+;
+
+  
+  //Slider interaction code goes below
